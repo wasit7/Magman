b996d2d02b1dea2ff29d9a93bf0dfdf49fe78381
PetchTU <phet02@gmail.com>
2016-04-01 14:48:27 +0000

    add_sentence_from_starwars

--- Speakable/Speakable.cc
+++ Speakable/Speakable.cc
@@ -9,13 +9,18 @@ void Speakable::addSentence(std::string _sentence){
 void Speakable::printSentence(){
     std::random_device rd;
     std::mt19937 gen(rd());
-    int i = gen() % sentence.size();
-    std::cout << sentence[i] << std::endl;
+    std::cout << sentence[gen() % sentence.size()] << std::endl;
 }
 
 Speakable::Speakable(){
     sentence.push_back("NOOB!!");
-    sentence.push_back("KAK");
-    sentence.push_back("KRIAN");
-    sentence.push_back("PUTANG INA MO!");
+    sentence.push_back("KAK!!");
+    sentence.push_back("KRIAN!!");
+    sentence.push_back("If you're not with me, then you're my enemy.");
+    sentence.push_back("You Underestimate My Power!!");
+    sentence.push_back("The force is strong with this one.");
+    sentence.push_back("I am your Father.");
+    sentence.push_back("Execute order 66.");
+    sentence.push_back("And now, young Skywalker... you will die.");
+    sentence.push_back("Are you going to kill me?");
 }
--- Speakable/Speakable.h
+++ Speakable/Speakable.h
@@ -1,5 +1,6 @@
 #include <vector>
 #include <string>
+
 class Speakable{
 public:
     std::vector<std::string> sentence;
