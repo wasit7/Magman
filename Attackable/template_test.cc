4cfaa86eaa166fbd02c45e937b356f3e95114740
PetchTU <phet02@gmail.com>
2016-04-03 20:53:17 +0000

    imp_template_example

--- Attackable/template_test.cc
+++ Attackable/template_test.cc
@@ -1,19 +1,32 @@
 #include <iostream>
+#include <string>
 
 template <class number>
 void display(number *i){std::cout << *i << std::endl;}
 
 class test{
-    friend std::ostream& operator<< (std::ostream& out,const test& data){return out << data.i;}
+    friend std::ostream& operator<< (std::ostream& out,const test& t){return out << t.i;}
 public:
     int i;
     test(int _i = 0){i = _i;}
 };
 
+template <class data>
+class test2{
+    friend std::ostream& operator<< (std::ostream& out,const test2& t){return out << t.i;}
+public:
+    data i;
+    test2(data _i){i = _i;}
+};
+
 int main(){
     int i = 1;
     test j = 2;
+    test2<int> k = 3;
+    test2<std::string> z = std::string("I'm lovin' it");
     display(&i);
     display(&j);
-    display(&"test");
+    display(&k);
+    display(&z);
+    display(&"bye!!");
 }
